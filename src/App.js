fe497626ab019539221419346b023c5f14d5da61
MaxShapovalov <55261865+MaxShapovalov@users.noreply.github.com>
2022-10-18 15:34:10 -0500

    removed comment from reactjs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,3 @@
-/* 
-  Implement a button which makes a get request to https://reqres.in/ to get a list of users and display them.
-  100% free reign to accomplish this goal however you wish, within the context of react.
-
-  apiMethods.js file has already been stubbed out for you. Feel free to use it or not.
-
-  ****Make any changes to this boilerplate that you want to.*****
-  ****The included code is only provided as a convienence.****
-
-  Bonus 1:  Add a button for each user to make a delete request to delete that user. 
-          Update the displayed users excluding the deleted user.
-
-  Bonus 2: Make a filter box to filter the displayed users by name.
-*/
 import axios from 'axios';
 import React,{useState, useEffect} from 'react';
 import './App.css';
